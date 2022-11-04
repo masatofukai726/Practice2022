c84b8bf5e2f695a269ce7f0a1e6be4c9f943dd6d
Masato Fukai <masato.fukai.0726@gmial.com>
2022-11-04 15:26:48 +0900

    Improve test command

--- test_plus.bash
+++ test_plus.bash
@@ -1,7 +1,19 @@
-#!/bin/bash
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2022 Masato Fukai
+# SPDX-License-Indentifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+res=0
+num=15
 
 out=$(seq 5 | ./plus)
-echo ${out}
+echo "Compared:"${num}
+echo "Caluculated:"${out}
 
-[ "${out}" = 15 ]
+[ "${out}" -eq "${num}" ] || ng $LINENO
 
+[ "$res" = 0 ] && echo "res: $?"
+exit $res
